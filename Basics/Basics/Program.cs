4ce02f500266f0d6441be2c71723a4b34007f782
stajduhar <99borna@gmail.com>
2024-07-25 22:53:58 +0200

    25.07.2024. Basics
    25.07.2024. Basics

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Xml.Schema;
+using System.Threading;
 
 namespace Basics
 {
@@ -1536,48 +1537,122 @@ namespace Basics
             //Console.WriteLine(Planets.Pluto + " is planet #" + (int)Planets.Pluto);
 
 
-            String name = PlanetRadius.Earth.ToString();
-            int radius = (int)PlanetRadius.Earth;
-            double volume = Volume(PlanetRadius.Earth);
+            //            String name = PlanetRadius.Earth.ToString();
+            //            int radius = (int)PlanetRadius.Earth;
+            //            double volume = Volume(PlanetRadius.Earth);
+            //
+            //            Console.WriteLine("planet: " + name);
+            //            Console.WriteLine("radius: " + radius + "km");
+            //            Console.WriteLine("volume:" + volume + "km^3");
+            //
+            //
+            //        }
+            //        public static double Volume(PlanetRadius radius)
+            //        {
+            //        }
+            //    }
+            //
+            //    enum Planets
+            //    {
+            //        Mecury = 1,
+            //        Venus = 2,
+            //        Earth = 3,
+            //        Mars = 4,
+            //        Jupiter = 5,
+            //        Saturn = 6,
+            //        Uranus = 7,
+            //        Neptune = 8,
+            //        Pluto = 9
+            //    }
+            //
+            //    enum PlanetRadius
+            //    {
+            //        Mecury = 2439,
+            //        Venus = 6051,
+            //        Earth = 6371,
+            //        Mars = 3389,
+            //        Jupiter = 69911,
+            //        Saturn = 58232,
+            //        Uranus = 25362,
+            //        Neptune = 24622,
+            //        Pluto = 1188
+            //    }
+            //
+            //
+            //            double volume = ( 4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+            //            return volume;
 
-            Console.WriteLine("planet: " + name);
-            Console.WriteLine("radius: " + radius + "km");
-            Console.WriteLine("volume:" + volume + "km^3");
+
+            // generics \\ =  "not specific to a particular data type"
+            //                add <T> to: classes, methods, fields etc... 
+            //                allows for code reusability for different data types
 
 
-        }
-        public static double Volume(PlanetRadius radius)
-        {
-            double volume = ( 4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
-            return volume;
-        }
-    }
 
-    enum Planets
-    {
-        Mecury = 1,
-        Venus = 2,
-        Earth = 3,
-        Mars = 4,
-        Jupiter = 5,
-        Saturn = 6,
-        Uranus = 7,
-        Neptune = 8,
-        Pluto = 9
-    }
+            //            int[] intArray = { 1, 2, 3 };
+            //            double[] doubleArray = { 1.0, 2.0, 3.0 };
+            //            string[] stringArray = { "1", "2", "3" };
+            //
+            //            displayElements(intArray);
+            //            displayElements(doubleArray);
+            //            displayElements(stringArray);
+            //        }
+            //
+            //            public static void displayElements<Thing>(Thing[] array)
+            //            {
+            //                foreach (Thing item in array)
+            //                {
+            //                    Console.Write(item + " ");
+            //                }
+            //            Console.WriteLine();
+            //        }
+            //
+            //    }
 
-    enum PlanetRadius
-    {
-        Mecury = 2439,
-        Venus = 6051,
-        Earth = 6371,
-        Mars = 3389,
-        Jupiter = 69911,
-        Saturn = 58232,
-        Uranus = 25362,
-        Neptune = 24622,
-        Pluto = 1188
-    }
+            // multithreading \\ 
+            // Thread = an execution path of a program
+            //          We can use multiple threads to preform,
+            //          different tasks of our program at the same time.
+            //          Current thread running is "main" thread
+            //          using System.Threading;
+
+//            Thread mainThread = Thread.CurrentThread;
+//            mainThread.Name = "Main Thread";
+//            //Console.WriteLine(mainThread.Name);
+//
+//            Thread thread1 = new Thread(() => CountDown("Timer #1"));
+//            Thread thread2 = new Thread(() => CountUp("Timer #2"));
+//            thread1.Start();
+//            thread2.Start();
+//
+//            Console.WriteLine(mainThread.Name + " is complete!");
+//
+//        }
+//
+//        public static void CountDown(String name)
+//        {
+//            for (int i = 10; i >= 0; i--)
+//            {
+//                Console.WriteLine("Timer #1 : " + i + " seconds");
+//                Thread.Sleep(1000);
+//            }
+//            Console.WriteLine("Timer #1 is complete!");
+//        }
+//
+//        public static void CountUp(String name)
+//        {
+//            for (int i = 0; i <= 10; i++)
+//            {
+//                Console.WriteLine("Timer #2 : " + i + " seconds");
+//                Thread.Sleep(1000);
+//            }
+//            Console.WriteLine("Timer #2 is complete!");
+//        }
+//
+//
+//
+//
+//    }
 
 
 }
